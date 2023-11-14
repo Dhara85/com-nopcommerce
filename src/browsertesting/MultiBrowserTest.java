63af69e162b641cf0f9a62d7e665f3e546c2ddae
Dhara85 <idharap07@gmail.com>
2023-11-14 00:46:35 +0000

    Initial commit

---
+++ src/browsertesting/MultiBrowserTest.java
@@ -0,0 +1,44 @@
+package browsertesting;
+/**
+ * Project-1 - ProjectName : com-nopcommerce
+ * BaseUrl = https://demo.nopcommerce.com/
+ * 1. Setup chrome browser.
+ * 2. Open URL.
+ * 3. Print the title of the page.
+ * 4. Print the current url.
+ * 5. Print the page source.
+ * 6. Navigate to Url.
+ * “https://demo.nopcommerce.com/login?returnUrl= %2F”
+ * 7. Print the current url.
+ * 8. Navigate back to the home page.
+ * 9. Navigate to the login page.
+ * 10. Print the current url.
+ * 11. Refresh the page.
+ * 12. Enter the email to email field.
+ * 13. Enter the password to password field.
+ * 14. Click on Login Button.
+ * 15. Close the browser.
+ */
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowserTest {
+    static String browser = "Chrome"; // declared globally
+    static String baseUrl = " https://demo.nopcommerce.com/ ";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("EdgeDriver")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+    }
+}
